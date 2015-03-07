fb69860d2d16148f53436b29ea8e90d35aee8747
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-07 19:44:58 +0100

    refs #17 Core setup.

--- lib/home_page/engine.rb
+++ lib/home_page/engine.rb
@@ -1,4 +1,9 @@
 module HomePage
   class Engine < ::Rails::Engine
+    config.autoload_paths << File.expand_path("../../../app/models/concerns", __FILE__)
+    config.autoload_paths << File.expand_path("../../../app/controllers/concerns", __FILE__)
+    config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
+    
+    config.generators{|g| g.orm :active_record }
   end
 end
