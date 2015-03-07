37297b0c40502536da1456179ee69ffa97254436
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-07 21:32:02 +0100

    refs #21 drop table users on rollback.

--- db/migrate/20150307152542_create_initial_schema.rb
+++ db/migrate/20150307152542_create_initial_schema.rb
@@ -47,8 +47,6 @@ class CreateInitialSchema < ActiveRecord::Migration
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    drop_table :users
   end
 end
