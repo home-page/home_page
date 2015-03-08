74c3aa53e2cfc86cfa3cb3c449e390acde55204c
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-08 19:49:00 +0100

    refs #17 Allow to customize home page stylesheets plus javascripts, toggle breadcrumbs and override templates through prepend_view_path (template inheritance).

--- app/controllers/home_page/application_controller.rb
+++ app/controllers/home_page/application_controller.rb
@@ -7,7 +7,29 @@ class HomePage::ApplicationController < ActionController::Base
   
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  helper_method :home_page_stylesheets, :home_page_javascripts, :resource
+
+  before_filter :custom_view_path
+
+  def home_page_stylesheets
+    @home_page_stylesheets || ['home_page/application']
+  end
+
+  def home_page_javascripts
+    @home_page_javascripts || ['home_page/application']
+  end
+
+  protected
+  
+  def show_breadcrumbs
+    @show_breadcrumbs = true
+  end
+
   private
+
+  def custom_view_path
+    prepend_view_path 'app/views/custom'
+  end
   
   def not_found(e)
     if Rails.env.development?
