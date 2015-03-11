fc3dc56ae954f428ec8cacc5cf32843394ce74d9
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-11 22:00:15 +0100

    refs #17 Removes alias for total pages and instead add a own accessor.

--- lib/home_page/pagination_metadata.rb
+++ lib/home_page/pagination_metadata.rb
@@ -1,7 +1,6 @@
 module HomePage
   class PaginationMetadata
-    attr_accessor :current_page, :per_page, :total_entries
-    alias_method :total_pages, :total_entries
+    attr_accessor :current_page, :per_page, :total_entries, :total_pages
     
     def initialize(attributes)
       attributes.each{|k,v| self.send("#{k}=", v) if self.respond_to?("#{k}=") }
