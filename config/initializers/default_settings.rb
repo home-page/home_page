52bcc8fe4bb6e5a59d1603a624c9424e1da36887
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-14 22:28:06 +0100

    refs #44 Adds settings tab for APIs and first API Volontari.at.

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,4 +1,7 @@
-Rails.cache.delete('settings:home_page.general.plugins')
 Setting.defaults['home_page.general.plugins'] = ['home_page']
-Rails.cache.delete('settings:home_page.general.navigation.items')
 Setting.defaults['home_page.general.navigation.items'] = ['users', 'settings', 'authentication']
+Setting.defaults['home_page.general.available_apis'] = ['volontariat']
+Setting.defaults['home_page.general.enabled_apis'] = []
+Setting.defaults['home_page.apis.providers.volontariat.hosts.development'] = 'http://localhost:3001'
+Setting.defaults['home_page.apis.providers.volontariat.hosts.production'] = 'http://Volontari.at'
+Setting.defaults['home_page.apis.providers.volontariat.user_id'] = 0
