e5510db4aa93126b8fbdd24c5bf031173c0ac371
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-14 22:27:29 +0100

    refs #29 Implements options required and label for FormHelper#setting_input.

--- app/helpers/home_page/form_helper.rb
+++ app/helpers/home_page/form_helper.rb
@@ -1,6 +1,9 @@
 module HomePage
   module FormHelper
-    def setting_input(f, var)
+    def setting_input(f, var, options = {})
+      required = options.delete(:required) || false
+      label = options.delete(:label)
+      
       default = Setting.defaults[var]
       hint_value = default.is_a?(Array) || default.is_a?(Hash) ? default.to_json : default
       value = @settings[var].try(:value)
@@ -11,8 +14,9 @@ module HomePage
       end
       
       f.input(
-        var, label: t("settings.vars.#{var}"), input_html: { id: var.gsub(/\./, '_'), value: value },
-        hint: Setting.defaults[var].nil? ? nil : t('settings.index.default_value', value: hint_value)
+        var, label: label || t("settings.vars.#{var}"), input_html: { id: var.gsub(/\./, '_'), value: value },
+        hint: Setting.defaults[var].nil? ? nil : t('settings.index.default_value', value: hint_value),
+        required: required
       )
     end
   end
